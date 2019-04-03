24fd5cbd9c8ab5ee3fcca2a3221ff1bec9a320f4
juliejosephGit <49228003+juliejosephGit@users.noreply.github.com>
2019-04-03 20:58:59 +0800

    Add files via upload

---
+++ UnitTestFDInterestRate/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestFDInterestRate")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestFDInterestRate")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1ddb8ee8-d430-44c3-83c2-1bd915ad037a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestFDInterestRate/UnitTest1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+using FinModel.ViewModel;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestFDInterestRate
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Valid_GetFDInterestRateListLastMonth()
+        {
+            DateTime lastmonth = DateTime.Now.AddMonths(-1);
+            MainVM mVM = new MainVM();
+            mVM.SelectedStartMonth = new FinModel.Model.Month(CultureInfo.CurrentCulture.DateTimeFormat.GetMonthName(lastmonth.Month), lastmonth.Month);
+            mVM.SelectedEndMonth = new FinModel.Model.Month(CultureInfo.CurrentCulture.DateTimeFormat.GetMonthName(lastmonth.Month), lastmonth.Month);
+            mVM.SelectedStartYear = lastmonth.Year;
+            mVM.SelectedEndYear = lastmonth.Year;
+            mVM.GetFDInterestRateList();
+            Assert.AreEqual(1, mVM.FDInterestRateList.Count);
+        }
+        [TestMethod]
+        public void Valid_GetFDInterestRateListYear()
+        {
+            DateTime lastYear = DateTime.Now.AddYears(-1);
+            MainVM mVM = new MainVM();
+            mVM.SelectedStartMonth = new FinModel.Model.Month(CultureInfo.CurrentCulture.DateTimeFormat.GetMonthName(lastYear.Month), lastYear.Month);
+            mVM.SelectedEndMonth = new FinModel.Model.Month(CultureInfo.CurrentCulture.DateTimeFormat.GetMonthName(DateTime.Now.Month), DateTime.Now.Month);
+            mVM.SelectedStartYear = lastYear.Year;
+            mVM.SelectedEndYear = DateTime.Now.Year;
+            mVM.GetFDInterestRateList();
+            Assert.AreEqual(true, mVM.FDInterestRateList.Count>0);
+        }
+    }
+}
